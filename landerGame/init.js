697e0bf8954e25e0a51d2e4cf3535c207f914a46
PelicanQ <77930606+PelicanQ@users.noreply.github.com>
2021-03-08 09:09:16 +0100

    Delete landerGame directory

--- landerGame/init.js
+++
@@ -1,6 +0,0 @@
-require(["state", "functions","constructors", "list", "tools", "mousedown", "mainScript",  "levels", "boxes" , "preData", "buttons", "boxDoers"], function(state, functions){
-	console.log("entered init");
-	//functions.windowResize();
-	state.draw(state.menu, true);
-	console.log("  aaa" + state.menu);
-})
